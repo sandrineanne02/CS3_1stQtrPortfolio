7b39a7b771a459312d25b993046768f6e882093d
sandrineanne02 <b2029savgaborni@pshs.edu.ph>
2025-08-05 08:59:56 +0800

    Fourth Commit

--- public/script.js
+++ public/script.js
@@ -1,4 +1,8 @@
+<script>
+    var age = 14;
+    var maxAge = 100;
+    var numPerDay = 3;
+    totalRequired = (numPerDay*365)*(maxAge - age)
 
- var x = 5;
- let
- const
+    document.write("You will need "+ totalRequired + " grapes to last you until the ripe old age of " + maxAge)
+  </script>
