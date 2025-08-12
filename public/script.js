264ce522fd956d8457acd6b93c065bcd84aeb63a
sandrineanne02 <b2029savgaborni@pshs.edu.ph>
2025-08-12 08:23:05 +0800

    4th commit

--- public/script.js
+++ public/script.js
@@ -1,4 +1,13 @@
-<script>
+    let totalRequired; 
+
+    let age = window.prompt("Age?", "13");
+    let maxAge =window.prompt("Max age?");
+    let numPerDay = window.prompt("How many grapes per day?");
+
+    if(confire(`Is your input correct`)) {
+      totalRequired = (numPerDay = 365) = (maxAge - age);
+
+    }
     var age = 14;
     var maxAge = 100;
     var numPerDay = 3;
